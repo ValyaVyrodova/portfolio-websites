0ea17b8feb4ae5b0fa0282a1e142892c2a9d510f
Kostya Vyrodov <kostya.vyrodov@gmail.com>
2021-11-23 23:02:12 +0200

    Create toggleHeart function

--- 4/app.js
+++ 4/app.js
@@ -4,26 +4,22 @@ const heartBtns = document.querySelectorAll(".heart")
 
 for (let index = 0; index < heartBtns.length; index++) {
     const heartBtn = heartBtns[index];
-    heartBtn.addEventListener("click", function (e) {
 
-        // e.target.style.backgroundColor = 'green'
+    heartBtn.addEventListener("click", function (e) {
         if (e.target.className === "heart") {
             const imgHeart = e.target.children[0]
-            if (imgHeart.src.includes('heart_1.png')) {
-                imgHeart.src = "./img/content/heart_2.png"
-            } else {
-                imgHeart.src = "./img/content/heart_1.png"
-            }
+            toggleHeart(imgHeart)
         } else {
             const imgHeart = e.target
-            if (imgHeart.src.includes('heart_1.png')) {
-                imgHeart.src = "./img/content/heart_2.png"
-            } else {
-                imgHeart.src = "./img/content/heart_1.png"
-            }
+            toggleHeart(imgHeart)
         }
     })
-
 }
 
-
+function toggleHeart(imgElement) {
+    if (imgElement.src.includes('heart_1.png')) {
+        imgElement.src = "./img/content/heart_2.png"
+    } else {
+        imgElement.src = "./img/content/heart_1.png"
+    }
+}
